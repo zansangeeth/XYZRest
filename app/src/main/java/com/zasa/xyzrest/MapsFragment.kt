aa19b4824ce467c9e9fa2cdc9175f815dd3946b1
zansangeeth <zansangeeth17@gmail.com>
2022-09-11 10:31:30 +0530

    completed all requirements

--- app/src/main/java/com/zasa/xyzrest/MapsFragment.kt
+++ app/src/main/java/com/zasa/xyzrest/MapsFragment.kt
@@ -1,17 +1,22 @@
 package com.zasa.xyzrest
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_maps.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MapsFragment : Fragment() {
 
@@ -25,9 +30,34 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val boundsBuilder = LatLngBounds.Builder()
+
+        val colombo = LatLng(6.905552, 79.850086)
+        boundsBuilder.include(colombo)
+        googleMap.addMarker(MarkerOptions().position(colombo).title("Branch in Colombo"))
+
+        val galle = LatLng(6.027569, 80.216585)
+        boundsBuilder.include(galle)
+        googleMap.addMarker(MarkerOptions().position(galle).title("Branch in galle"))
+
+        val kandy = LatLng(7.2915073, 80.6373148)
+        boundsBuilder.include(kandy)
+        googleMap.addMarker(MarkerOptions().position(kandy).title("Branch in kandy"))
+
+        val anuradhapura = LatLng(8.3339382, 80.4103734)
+        boundsBuilder.include(anuradhapura)
+        googleMap.addMarker(MarkerOptions().position(anuradhapura).title("Branch in anuradhapura"))
+
+        val negombo = LatLng(7.2124591, 79.8442834)
+        boundsBuilder.include(negombo)
+        googleMap.addMarker(MarkerOptions().position(negombo).title("Branch in negombo"))
+
+        val jaffna = LatLng(9.6675924, 80.0165518)
+        boundsBuilder.include(colombo)
+        googleMap.addMarker(MarkerOptions().position(jaffna).title("Branch in jaffna"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 1000, 1000, 0))
+
     }
 
     override fun onCreateView(
